ddc3c72ba2d92a9163799ab3be26544ed50e59ce
Yajie Xue <yajiex@outlook.com>
2016-05-25 10:54:36 +0800

    update package.json, we will use pm run build to run the project and don't need to type webpack-dev-server --progress --colors every time

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 var path = require('path');
 var webpack = require('webpack');
- 
+
 module.exports = {
   entry: './index.jsx',
-  output: { path: __dirname, filename: 'bundle.js' },
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
+        test: /\.jsx$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
